72e97cae7a2cca7c355a248618cace0ca6b879e9
AnPQ <anpham1122000@gmail.com>
2025-05-10 23:51:50 +0700

    fix call api cart and do not show cart badge when logout

--- shopee/src/components/Header/Header.tsx
+++ shopee/src/components/Header/Header.tsx
@@ -44,7 +44,8 @@ const Header = () => {
 
   const { data: purchaseInCartData } = useQuery({
     queryKey: [QueryKeys.purchases, { status: purchaseStatus.inCart }],
-    queryFn: () => purchaseApi.getPurchases({ status: purchaseStatus.inCart })
+    queryFn: () => purchaseApi.getPurchases({ status: purchaseStatus.inCart }),
+    enabled: isAuthenticated
   })
   const purchasesInCart = purchaseInCartData?.data.data
 
@@ -266,9 +267,11 @@ const Header = () => {
                     d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
                   />
                 </svg>
-                <span className='absolute left-[17px] top-[-5px] rounded-full bg-white px-[9px] py-[1.5px] text-xs text-orange'>
-                  {purchasesInCart?.length}
-                </span>
+                {purchasesInCart && (
+                  <span className='absolute left-[17px] top-[-5px] rounded-full bg-white px-[9px] py-[1.5px] text-xs text-orange'>
+                    {purchasesInCart?.length}
+                  </span>
+                )}
               </Link>
             </Popover>
           </div>

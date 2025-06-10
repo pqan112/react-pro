1b6874fcbfca7ca8edfeef5febace4e71eb327d8
PQA <pqan112@gmail.com>
2025-06-10 15:23:56 +0700

    upload profile p3

--- shopee/src/apis/user.api.ts
+++ shopee/src/apis/user.api.ts
@@ -8,6 +8,13 @@ const userApi = {
   },
   updateProfile(body: BodyUpdateProfile) {
     return http.put<SuccessResponse<User>>('user', body)
+  },
+  uploadAvatar(body: FormData) {
+    return http.post<SuccessResponse<string>>('user/upload-avatar', body, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
   }
 }
 
--- shopee/src/pages/User/pages/Profile/Profile.tsx
+++ shopee/src/pages/User/pages/Profile/Profile.tsx
@@ -2,51 +2,56 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { useContext, useEffect, useMemo, useRef, useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
 import userApi from 'src/apis/user.api'
 import Button from 'src/components/Button'
 import Input from 'src/components/Input'
 import InputNumber from 'src/components/InputNumber'
 import { QueryKeys } from 'src/constants/queryKey'
 import { AppContext } from 'src/contexts/app.context'
-import { userSchema, UserSchema } from 'src/utils/rules'
-import DateSelect from '../../components/DateSelect'
-import { toast } from 'react-toastify'
+import { ErrorResponse } from 'src/types/utils.type'
 import { setProfileToLocalStorage } from 'src/utils/auth'
-import http from 'src/utils/http'
-import { SuccessResponse } from 'src/types/utils.type'
-import { Status, UploadStatus } from 'src/constants/status.enum'
-import { AxiosProgressEvent } from 'axios'
+import { userSchema, UserSchema } from 'src/utils/rules'
+import { isAxiosUnprocessableEntityError } from 'src/utils/utils'
+import DateSelect from '../../components/DateSelect'
 
 // Flow 1:
 // Nhấn upload: upload lên server luôn => server trả về url ảnh
 // Nhấn submit thì gửi url ảnh cộng với data lên server
+// user spam gửi nhiều hình lên server
+// get progress and status
 
 // Flow 2:
 // Nhấn upload: không upload lên server
 // Nhấn submit thì tiến hành upload lên server, nếu upload thành công thì tiến hành gọi api updateProfile
 
 type FormData = Pick<UserSchema, 'name' | 'address' | 'phone' | 'date_of_birth' | 'avatar'>
+type FormDataError = Omit<FormData, 'date_of_birth'> & {
+  date_of_birth?: string
+}
 const profileSchema = userSchema.pick(['name', 'address', 'phone', 'date_of_birth', 'avatar'])
 export default function Profile() {
   const fileInputRef = useRef<HTMLInputElement>(null)
-  const [file, setFile] = useState<File>()
-  const [status, setStatus] = useState<UploadStatus>(Status.Idle)
-  const [progress, setProgress] = useState<number>(0)
+  const [file, setFile] = useState<File | null>(null)
+  // const [status, setStatus] = useState<UploadStatus>(Status.Idle)
+  // const [progress, setProgress] = useState<number>(0)
   const { setProfile } = useContext(AppContext)
-  const { data: profileData, refetch } = useQuery({
+  const { data: profileData } = useQuery({
     queryKey: [QueryKeys.profile],
     queryFn: userApi.getProfile
   })
+
+  console.log('profileData', profileData)
   const profile = profileData?.data.data
-  const previewImage = useMemo(() => (file ? URL.createObjectURL(file) : ''), [file])
   const {
     register,
     control,
-    formState: { errors },
+    formState: { errors, isDirty },
     handleSubmit,
     watch,
     setValue,
-    reset
+    reset,
+    setError
   } = useForm<FormData>({
     defaultValues: {
       name: '',
@@ -57,8 +62,14 @@ export default function Profile() {
     },
     resolver: yupResolver(profileSchema)
   })
-
   const avatar = watch('avatar')
+  console.log('isDirty', isDirty)
+  const previewImage = useMemo(() => {
+    if (file) {
+      return URL.createObjectURL(file)
+    }
+    return avatar
+  }, [file, avatar])
 
   useEffect(() => {
     if (profile) {
@@ -74,85 +85,92 @@ export default function Profile() {
     }
   }, [profile, reset])
 
-  const uploadAvatar = async (file?: File) => {
-    if (!file) return
-    setStatus(Status.Loading)
-    setProgress(0)
-    const formData = new FormData()
-    formData.append('image', file)
-    try {
-      const res = await http.post<SuccessResponse<string>>('user/upload-avatar', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        },
-        onUploadProgress: (progressEvent: AxiosProgressEvent) => {
-          const progress = progressEvent.total
-            ? Math.round((progressEvent.loaded * 100) / progressEvent.total)
-            : 0
-          setProgress(progress)
-        }
-      })
-      setValue('avatar', res.data.data)
-      setStatus(Status.Success)
-      setProgress(100)
-    } catch (error) {
-      console.log('error', error)
-      setStatus(Status.Error)
-      setProgress(0)
-    }
-  }
+  // get progess upload image
+  // const uploadAvatar = async () => {
+  //   if (!file) return
+  //   setStatus(Status.Loading)
+  //   setProgress(0)
+  //   const formData = new FormData()
+  //   formData.append('image', file)
+  //   try {
+  //     const res = await http.post<SuccessResponse<string>>('user/upload-avatar', formData, {
+  //       headers: {
+  //         'Content-Type': 'multipart/form-data'
+  //       },
+  //       onUploadProgress: (progressEvent: AxiosProgressEvent) => {
+  //         const progress = progressEvent.total
+  //           ? Math.round((progressEvent.loaded * 100) / progressEvent.total)
+  //           : 0
+  //         setProgress(progress)
+  //       }
+  //     })
+  //     setValue('avatar', res.data.data)
+  //     setStatus(Status.Success)
+  //     setProgress(100)
+  //   } catch (error) {
+  //     console.log('error', error)
+  //     setStatus(Status.Error)
+  //     setProgress(0)
+  //   }
+  // }
 
   const updateProfileMutation = useMutation({
     mutationFn: userApi.updateProfile
   })
-  // const uploadAvatarMutaion = useMutation({
-  //   mutationFn: userApi.uploadAvatar
-  // })
-  // const {
-  //   register,
-  //   control,
-  //   formState: { errors },
-  //   handleSubmit,
-  //   setValue,
-  //   watch,
-  //   setError
-  // } = methods
+  const uploadAvatarMutaion = useMutation({
+    mutationFn: userApi.uploadAvatar
+  })
 
-  // const avatar = watch('avatar')
-
-  // useEffect(() => {
-  //   if (profile) {
-  //     setValue('name', profile.name || '')
-  //     setValue('phone', profile.phone || '')
-  //     setValue('address', profile.address || '')
-  //     setValue('avatar', profile.avatar || '')
-  //     setValue(
-  //       'date_of_birth',
-  //       profile.date_of_birth ? new Date(profile.date_of_birth) : new Date(1990, 0, 1)
-  //     )
-  //   }
-  // }, [profile, setValue])
   const onSubmit = handleSubmit(async (data) => {
     try {
+      if (!file) return
+      let avatarName = avatar
+      const form = new FormData()
+      form.append('image', file)
+      const uploadRes = await uploadAvatarMutaion.mutateAsync(form)
+      avatarName = uploadRes.data.data
+      setValue('avatar', avatarName)
       const res = await updateProfileMutation.mutateAsync({
         ...data,
-        date_of_birth: data.date_of_birth?.toISOString()
+        date_of_birth: data.date_of_birth?.toISOString(),
+        avatar: avatarName
       })
-      refetch()
-      toast.success(res.data.message)
       setProfile(res.data.data)
       setProfileToLocalStorage(res.data.data)
-    } catch (error) {}
+      toast.success(res.data.message)
+    } catch (error) {
+      if (isAxiosUnprocessableEntityError<ErrorResponse<FormDataError>>(error)) {
+        const formError = error.response?.data.data
+        if (formError) {
+          Object.keys(formError).forEach((key) => {
+            setError(key as keyof FormDataError, {
+              message: formError[key as keyof FormDataError],
+              type: 'Server'
+            })
+          })
+        }
+      }
+    }
   })
 
   const handleChangeFile = (event: React.ChangeEvent<HTMLInputElement>) => {
     const fileFromLocal = event.target.files?.[0]
-    setFile(fileFromLocal)
-    uploadAvatar(fileFromLocal)
+    if (fileFromLocal) {
+      setFile(fileFromLocal)
+    }
   }
 
   const handleUpload = () => {
     fileInputRef.current?.click()
+  }
+
+  const handleRemoveAvatar = () => {
+    setFile(null)
+    setValue('avatar', '')
+    // fix behavior xóa image cũ rồi không chọn được image đó nữa -> set value của input field là string rỗng
+    if (fileInputRef.current) {
+      fileInputRef.current.value = ''
+    }
   }
 
   return (
@@ -227,14 +245,13 @@ export default function Profile() {
               />
             )}
           />
-
-          {/* <DateSelect /> */}
           <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
             <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right' />
             <div className='sm:w-[80%] sm:pl-5'>
               <Button
                 className='flex h-9 items-center rounded-sm bg-orange px-5 text-center text-sm text-white hover:bg-orange/80'
                 type='submit'
+                disabled={!isDirty}
               >
                 Lưu
               </Button>
@@ -244,30 +261,29 @@ export default function Profile() {
         <div className='flex justify-center md:w-72 md:border-l md:border-l-gray-200'>
           <div className='flex flex-col items-center'>
             <div className='relative my-5 h-24 w-24'>
-              {status === Status.Success && (
+              {previewImage ? (
                 <button
                   className='absolute top-0 right-0 h-5 w-5 rounded-full bg-red-600 text-white'
                   type='button'
-                  // onClick={}
+                  onClick={handleRemoveAvatar}
                 >
                   x
                 </button>
-              )}
+              ) : null}
               <img
-                src={previewImage || avatar}
-                alt=''
+                src={previewImage}
+                alt='avatar'
                 className='h-full w-full rounded-full object-cover'
               />
             </div>
-            {status === Status.Loading && (
+            {/* {status === Status.Loading && (
               <div className='mb-3 h-2 w-[100px] rounded-md border border-gray-300'>
                 <div
                   className='h-full rounded-md bg-blue-600'
                   style={{ width: `${progress}px` }}
                 ></div>
               </div>
-            )}
-            {/* <InputFile onChange={handleChangeFile} /> */}
+            )} */}
             <input
               className='hidden'
               type='file'
